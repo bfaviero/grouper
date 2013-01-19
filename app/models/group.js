788ee1b6f641cf97cb46ddb6c0a05cfb7f401269
Eric Bakan <eric@ebakan.com>
2013-01-19 02:22:22 -0500

    Added nice json hook to group model

--- app/models/group.js
+++ app/models/group.js
@@ -9,4 +9,8 @@ var GroupSchema = new Schema({
     lastused: {type: Date, default: Date.now, required: true}
 });
 
+UserSchema.virtual('json').get(function() {
+    return "{'name': '"+this.name+"', 'lat': "+this.loc[1]+", 'lon': "+this.loc[0]+", 'private': "+Boolean(this.pin !=== undefined)+", 'lastused': "+this.lastused.getTime()+"}";
+});
+
 module.exports = mongoose.model('Group', GroupSchema);
