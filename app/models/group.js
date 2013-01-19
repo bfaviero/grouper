329c644ddf6f9810bf8a84a847a847cc47fa179c
Eric Bakan <eric@ebakan.com>
2013-01-19 02:45:32 -0500

    Fixed dumb errors

--- app/models/group.js
+++ app/models/group.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var GroupSchema = new Schema({
+    _user: {type: Schema.ObjectId, ref: 'User'},
     name: {type: String, required: true},
     pin: {type: String},
     loc: {type: [Number], index: '2d', required: true},
@@ -9,8 +10,8 @@ var GroupSchema = new Schema({
     lastused: {type: Date, default: Date.now, required: true}
 });
 
-UserSchema.virtual('json').get(function() {
-    return "{'name': '"+this.name+"', 'lat': "+this.loc[1]+", 'lon': "+this.loc[0]+", 'private': "+Boolean(this.pin !=== undefined)+", 'lastused': "+this.lastused.getTime()+"}";
+GroupSchema.virtual('json').get(function() {
+    return "{'name': '"+this.name+"', 'lat': "+this.loc[1]+", 'lon': "+this.loc[0]+", 'private': "+Boolean(this.pin !== undefined)+", 'lastused': "+this.lastused.getTime()+"}";
 });
 
 module.exports = mongoose.model('Group', GroupSchema);
